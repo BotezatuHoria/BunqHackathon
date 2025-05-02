13ec29d25d76752fae346e43b3e4771599b00112
Botezatu Horia <horia12btz@gmail.com>
2025-05-03 01:50:21 +0200

    Changes to API

--- ai_agent.py
+++ ai_agent.py
@@ -11,15 +11,37 @@ client = OpenAI(
   api_key = nvidia_key
 )
 
-completion = client.chat.completions.create(
-  model="meta/llama-3.3-70b-instruct",
-  messages=[{"role":"user","content":""}],
-  temperature=0.2,
-  top_p=0.7,
-  max_tokens=1024,
-  stream=True
-)
+def get_agent_answer(transactions, persona_data, prompt) :
+  system_prompt = (
+    "You are a travel assistant that helps users plan budget-conscious vacations. "
+    "You consider the user's recent transactions, financial behavior, current savings, and travel preferences. "
+    "Offer realistic suggestions for destinations, links to hotels, costs, number of days, and transportation (plane tickets with links). "
+    "Tailor it to the user profile and finances."
+  )
 
-for chunk in completion:
-  if chunk.choices[0].delta.content is not None:
-    print(chunk.choices[0].delta.content, end="")
+  user_content = f"""
+      Persona:
+      {persona_data}
+
+      Transactions:
+      {transactions}
+
+      Prompt:
+      {prompt}
+      """
+
+  completion = client.chat.completions.create(
+    model="meta/llama-3.3-70b-instruct",
+    messages=[
+      {"role": "system", "content": system_prompt},
+      {"role": "user", "content": user_content}
+    ],
+    temperature=0.2,
+    top_p=0.7,
+    max_tokens=1024,
+    stream=True
+  )
+
+  for chunk in completion:
+    if chunk.choices[0].delta.content is not None:
+      print(chunk.choices[0].delta.content, end="")
--- api_routing.py
+++ api_routing.py
@@ -1,6 +1,8 @@
 from contextlib import nullcontext
 
 from fastapi import FastAPI
+from util_transactions import get_all_transactions
+from ai_agent import  get_agent_answer
 
 app = FastAPI()
 
@@ -9,5 +11,25 @@ def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/users/{id_user}/transactions")
-def read_user_transactions(id_user: int):
-    return nullcontext
+async def read_user_transactions(id_user: int):
+    payments = get_all_transactions(id_user)
+
+    if not payments:
+        print("No transactions foundsssss.")
+        return {"transactions" : {}}
+
+    result = []
+    for payment in payments :
+        result.append({
+            "id": payment.id_,
+            "amount": {
+                "value": payment.amount.value,
+                "currency": payment.amount.currency
+            },
+            "description": payment.description,
+            "created": payment.created.isoformat() if hasattr(payment.created, "isoformat") else str(payment.created)
+        })
+
+    get_agent_answer(result)
+
+    return {"transactions": result}
